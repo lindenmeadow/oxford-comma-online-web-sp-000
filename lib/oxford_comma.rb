7379ac6f282bd0a6d5bf6176f997540b5b2702f3
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-31 21:49:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-  array.join(", ")
+  def oxford_comma(array)
+  if array.length == 1
+    array[0].to_s
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    array.join(", ")
+  end
+end
 end
