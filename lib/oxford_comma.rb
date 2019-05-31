0ecd9c6c316268342ad108e452d79dcd4bb4209e
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-31 22:02:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,9 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length >= 3
+    i = "and "
+    x = i + array[-1]
+    array[-1] = x
     array.join(", ")
   end
 end
