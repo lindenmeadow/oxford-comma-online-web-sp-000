ae95974fe5ef20a4d14a7e56ae16d1da76f005c7
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-31 22:03:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0].to_str
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    i = "and "
+    x = i + array[-1]
+    array[-1] = x
+    array.join(", ")
+  end
 end
