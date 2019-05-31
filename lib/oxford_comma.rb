f7901940f64cfdd5cc50efed73b8b53794d5b16e
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-31 21:50:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   def oxford_comma(array)
   if array.length == 1
-    array[0].to_s
+    array.to_s
   elsif array.length == 2
     array.join(" and ")
   elsif array.length >= 3
